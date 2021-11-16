bc85dc01de425d0cf907ecf7ecf14c9953cdf727
Lakshmi Morampudi <lmorampudi@horne.com>
2021-11-16 09:37:57 -0600

    Added TopicSender and SubscriptionReciever

---
+++ SubscriptionReceiver/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using static System.Console;
+
+namespace SubscriptionReceiver
+{
+    class Program
+    {
+        // connection string to your Service Bus namespace
+        static string connectionString = "Endpoint=sb://hornegsdevmsg01.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=bC2o1GygY2qHq9Sk7XYG3UahkfSh4gwdgCTr3JvCkJg=";
+        // name of the Service Bus topic
+        static string topicName = "messagingservice";
+
+        // name of the subscription to the topic
+        static string subscriptionName = "TextMessage";
+
+        // the client that owns the connection and can be used to create senders and receivers
+        static ServiceBusClient client;
+
+        // the processor that reads and processes messages from the subscription
+        static ServiceBusProcessor processor;
+
+        // handle received messages
+        static async Task MessageHandler(ProcessMessageEventArgs args)
+        {
+            string body = args.Message.Body.ToString();
+            Console.WriteLine($"Received: {body} from subscription: {subscriptionName}");
+            //throw new InvalidOperationException();
+
+            // complete the message. messages is deleted from the subscription. 
+             await args.CompleteMessageAsync(args.Message);
+        }
+
+        // handle any errors when receiving messages
+        private static async Task ErrorHandler(ProcessErrorEventArgs errorEventArgs)
+        {
+            Console.WriteLine(errorEventArgs.Exception.ToString());
+            
+            await Out.WriteLineAsync($"Exception: {errorEventArgs.Exception}");
+            await Out.WriteLineAsync($"FullyQualifiedNamespace: {errorEventArgs.FullyQualifiedNamespace}");
+            await Out.WriteLineAsync($"ErrorSource: {errorEventArgs.ErrorSource}");
+            await Out.WriteLineAsync($"EntityPath: {errorEventArgs.EntityPath}");
+            //return Task.CompletedTask;
+        }
+
+        static async Task Main()
+        {
+            // The Service Bus client types are safe to cache and use as a singleton for the lifetime
+            // of the application, which is best practice when messages are being published or read
+            // regularly.
+            //
+            // Create the clients that we'll use for sending and processing messages.
+            client = new ServiceBusClient(connectionString);
+            var options = new ServiceBusProcessorOptions
+            {
+
+                AutoCompleteMessages = false,
+                MaxConcurrentCalls = 20,
+                MaxAutoLockRenewalDuration = TimeSpan.FromMinutes(10),
+                ReceiveMode = ServiceBusReceiveMode.PeekLock,
+                PrefetchCount = 20
+            };
+
+            // create a processor that we can use to process the messages
+            processor = client.CreateProcessor(topicName, subscriptionName, options);
+
+            try
+            {
+                // add handler to process messages
+                processor.ProcessMessageAsync += MessageHandler;
+
+                // add handler to process any errors
+                processor.ProcessErrorAsync += ErrorHandler;
+
+                // start processing 
+                await processor.StartProcessingAsync();
+
+                Console.WriteLine("Wait for a minute and then press any key to end the processing");
+                Console.ReadKey();
+
+                // stop processing 
+                Console.WriteLine("\nStopping the receiver...");
+                await processor.StopProcessingAsync();
+                Console.WriteLine("Stopped receiving messages");
+            }
+            finally
+            {
+                // Calling DisposeAsync on client types is required to ensure that network
+                // resources and other unmanaged objects are properly cleaned up.
+                await processor.DisposeAsync();
+                await client.DisposeAsync();
+            }
+        }
+    }
+}
---
+++ TopicSender/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json.Linq;
+
+namespace TopicSender
+{
+    class Program
+    {
+        // connection string to your Service Bus namespace
+        static string connectionString = "Endpoint=sb://hornegsdevmsg01.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=bC2o1GygY2qHq9Sk7XYG3UahkfSh4gwdgCTr3JvCkJg=";
+
+        // name of your Service Bus topic
+        static string topicName = "messagingservice";
+
+        // the client that owns the connection and can be used to create senders and receivers
+        static ServiceBusClient client;
+
+        // the sender used to publish messages to the topic
+        static ServiceBusSender sender;
+
+        // number of messages to be sent to the topic
+        private const int numOfMessages = 100;
+        static async Task Main()
+            {
+            // The Service Bus client types are safe to cache and use as a singleton for the lifetime
+            // of the application, which is best practice when messages are being published or read
+            // regularly.
+            //
+            // Create the clients that we'll use for sending and processing messages.
+            try
+            {
+                client = new ServiceBusClient(connectionString);
+                sender = client.CreateSender(topicName);
+
+                // create a batch 
+                ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+                int start = 1;
+                int end = numOfMessages;
+                for (var i = 1; i <= numOfMessages; i++)
+                {
+                    var message = new ServiceBusMessage("{'account_id':'" + i + ",'message':'static text message'}")
+                    {
+                        Subject = "Voice"
+                    };
+                    // if number is odd send message with SMS subject to be consumed by S1
+                    if (i % 2 != 0)
+                    {
+                        message = new ServiceBusMessage("{'account_id':'" + i + ",'message':'static text message'}")
+                        {
+                            Subject = "SMS"
+                        };
+                    }
+                      
+
+                    // Can add additinal properties
+                   // message = new ServiceBusMessage("I'm rich! I have 1000");
+                   // message.ApplicationProperties.Add("currency", "CHF");
+
+                    // try adding a message to the batch
+                    if (!messageBatch.TryAddMessage(message))
+                    {
+                        if (messageBatch.Count == 0)
+                        {
+                            Console.WriteLine($"Failed to fit message number in a batch {i}");
+                            break;
+                        }
+                        end = --i;
+                        
+
+                        Console.WriteLine($"Batch sent - {start} to {end} messages");
+
+                        start = end+1;
+                        // Decrement counter so that message number i can get another chance in a new batch
+                        
+                        // Use the producer client to send the batch of messages to the Service Bus topic
+                        await sender.SendMessagesAsync(messageBatch);
+                        messageBatch.Dispose();
+                        Console.WriteLine($"A batch of {numOfMessages} messages has been published to the topic.");
+
+                        //create a new batch
+
+                        messageBatch = await sender.CreateMessageBatchAsync();
+                    }
+                    else if (i == numOfMessages)
+                    {
+
+                        // send the final batch
+                        await sender.SendMessagesAsync(messageBatch);
+                        Console.WriteLine($"Batch sent - {start} to {i} messages");
+                        messageBatch.Dispose();
+                    }
+                }
+            }
+            finally
+            {
+                // Calling DisposeAsync on client types is required to ensure that network
+                // resources and other unmanaged objects are properly cleaned up.
+                await sender.DisposeAsync();
+                await client.DisposeAsync();
+            }
+            Console.WriteLine("Press any key to end the application");
+            Console.ReadKey();
+
+        }
+       
+    }
+    
+}
